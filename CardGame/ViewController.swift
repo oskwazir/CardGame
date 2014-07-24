457bd6c07cbe94ef8874b8cc12d177c118379671
Omer Wazir <omer.wazir@gmail.com>
2014-07-24 00:42:20 -0700

    Got front and back of card to flip. Layout is acting weird. Is it Xcode beta or some attribute I'm not noticing?

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBAction func touchCardButton(sender: UIButton) {
+        
+        if !sender.currentTitle.isEmpty{
+            sender.setBackgroundImage(UIImage(named: "cardBack"), forState: UIControlState.Normal)
+            sender.setTitle("", forState: UIControlState.Normal)
+        } else {
+            sender.setBackgroundImage(UIImage(named: "cardFront"), forState: UIControlState.Normal)
+            sender.setTitle("A♥︎", forState: UIControlState.Normal)
+        }
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
